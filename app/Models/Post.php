f54ddd2849644de145abc973591b27aee20e6c2b
Rina-Matsumoto <ri0322@stu.kanazawa-u.ac.jp>
2023-07-06 16:46:14 +0900

    取得件数制限とページネーション

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    public function getByLimit(int $limit_count = 1)
+    {
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+        return $this->orderby('updated_at', 'DESC')->paginate($limit_count);
+    
+    }
 }
